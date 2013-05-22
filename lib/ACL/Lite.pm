5ae5663af3fd7e0b201577aa35c72d542778a47d
Stefan Hornburg (Racke) <racke@linuxia.de>
2013-05-22 15:33:21 +0200

    Update copyright year.

--- lib/ACL/Lite.pm
+++ lib/ACL/Lite.pm
@@ -207,7 +207,7 @@ L<http://search.cpan.org/dist/ACL-Lite/>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2011 Stefan Hornburg (Racke).
+Copyright 2011-2013 Stefan Hornburg (Racke).
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
